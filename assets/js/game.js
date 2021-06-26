55933d1d4e9c8884e30126e8f555a3aa2e7ae3eb
Your Name <you@example.com>
2021-06-25 18:06:09 -0700

    added while loop to fight all robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,10 +8,11 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators");
 
+    while (enemyHealth > 0){
     var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
+    
     // if player choses to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
 
@@ -59,8 +60,11 @@ var fight = function(enemyName) {
     else {
         window.alert("You need to choose a valid option. Try again!");
     }
+  }
 }
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
