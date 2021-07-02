5da7f13b624d9eb43a8fe60e90710cf11b11096d
Your Name <you@example.com>
2021-07-01 18:35:03 -0700

    bug fix in fight or skip feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,38 @@
+var fightOrSkip = function () {
+    var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+    // conditional Recursive function call 
+    
+    if (promptFight === "" || promptFight === null) {
+        window.alert("You need to provide a valid answer! Please try again.");
+        return fightOrSkip();
+    }
+
+    promptFight = promptFight.toLowerCase();
+    if (promptFight === "skip") {
+        // confirm that the player wants to skip
+        var confirmSkip = window.confirm("Are you sure you want to skip?");
+        // if yes, leave fight
+
+        if (confirmSkip) {
+            window.alert(playerInfo.name + " has chosen to skip the fight. Goodbye!");
+            //  subtract money for skipping 
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
+            
+            // return true if player wants to leave
+            return true;
+        }
+    }
+    return false;
+}
+
 var fight = function (enemy) {
 
     while (playerInfo.health > 0 && enemy.health > 0) {
-        var promptFight = window.prompt(" Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-
-        if (promptFight === "skip" || promptFight === "SKIP") {
-            // confirm that the player wants to skip
-            var confirmSkip = window.confirm("Are you sure you want to skip?");
-            // if yes, leave fight
-            if (confirmSkip) {
-                window.alert(playerInfo.name + " has chosen to skip the fight. Goodbye!");
-                //  subtract money for skipping 
-                playerInfo.money = Math.max(0, playerInfo.money - 10);
-                console.log("playerInfo.money", playerInfo.money);
-                break;
-            }
+        if (fightOrSkip()) { 
+            // if true, leave fight by breaking loop 
+            break;
         }
-
         //Subtract the value of `playerInfo.attack` from the value of `enemy.health` and use that result to update the value in the `enemy.health` variable
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack)
         enemy.health = Math.max(0, enemy.health - playerInfo.attack);
@@ -158,13 +175,13 @@ var randomNumber = function (min, max) {
 // function to set name  
 var getPlayerName = function () {
     var name = "";
-    
+
     while (name === "" || name === null) {
         name = prompt("What is your robot's name?");
     }
 
     console.log(" Your robot's name is" + name);
-    return name; 
+    return name;
 };
 
 var playerInfo = {
